b60329af3d3ed917fb0dfddd485723acbfcde331
ctsirka <xarat19@gmail.com>
2023-12-26 12:57:41 +0200

    Add Costs, Types in db

--- SQL/InsertCars.sql
+++ SQL/InsertCars.sql
@@ -1,4 +1,4 @@
-Insert into  LOCATION (Address) VAlUES ("Volos");
+Insert into  PARKING_LOCATION (Address) VAlUES ("Volos");
 
 Insert into VEHICLE (X_Coordinates,	Y_Coordinates,Available,Passenger_capacity,	Gearbox,Brand,Color,Model,Type,	Fuel_Type,Plate_number,FK1_Address)
 VAlUES (39.375789, 22.960016, TRUE, 5, TRUE, "Renault", "Grey", "CAPTUR", "SUV", "Diesel", "ION8888", "Volos");
