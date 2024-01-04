b8dbf2e6f9504c962f6c61eb32f6ebec19b4bec3
pravramidis <pravramidis@gmail.com>
2024-01-04 15:13:51 +0200

    Add prices

--- SQL/InserFinalCars.sql
+++ SQL/InserFinalCars.sql
@@ -13,16 +13,16 @@ Insert into VEHICLE (Y_Coordinates,	X_Coordinates,Available,Passenger_capacity,	
 VAlUES (39.374556, 22.934180, TRUE, 5, TRUE, "Opel", "Mokka","Green","SUV", "Gasoline", "UPT3445", "Volos");
 
 Insert into VEHICLE (Y_Coordinates,	X_Coordinates,Available,Passenger_capacity,	Gearbox,Color,Brand,Model,Type,	Fuel_Type,Plate_number,FK1_Address)
-VAlUES (39.364992, 22.956323, TRUE, 5, TRUE, "Renault", "Clio","Blue","Supermini", "Gasoline", "ITP2345", "Volos");
+VAlUES (39.364992, 22.956323, TRUE, 5, TRUE, "Renault", "Clio","Blue","Supermini", "Gasoline", "OIP3345", "Volos");
 
 Insert into VEHICLE (Y_Coordinates,	X_Coordinates,Available,Passenger_capacity,	Gearbox,Color,Brand,Model,Type,	Fuel_Type,Plate_number,FK1_Address)
 VAlUES (39.360627, 22.930577, TRUE, 2, TRUE, "Lamborghini", "Hurracan","Yellow","Sport", "Gasoline", "NON5445", "Volos");
 
 Insert into VEHICLE (Y_Coordinates,	X_Coordinates,Available,Passenger_capacity,	Gearbox,Color,Brand,Model,Type,	Fuel_Type,Plate_number,FK1_Address)
-VAlUES (39.363477, 22.956677, TRUE, 8, TRUE, "Opel", "Zafira","Orange","Van", "Gasoline", "IPP2345", "Volos");
+VAlUES (39.363477, 22.956677, TRUE, 8, FALSE, "Opel", "Zafira","Orange","Van", "Gasoline", "IPP2345", "Volos");
 
 Insert into VEHICLE (Y_Coordinates,	X_Coordinates,Available,Passenger_capacity,	Gearbox,Color,Brand,Model,Type,	Fuel_Type,Plate_number,FK1_Address)
-VAlUES (39.361911, 22.959715, TRUE, 8, TRUE, "Opel", "Zafira","Orange","Van", "Gasoline", "IHG8767", "Volos");
+VAlUES (39.361911, 22.959715, TRUE, 8, FALSE, "Opel", "Zafira","Orange","Van", "Gasoline", "IHG8767", "Volos");
 
 Insert into VEHICLE (Y_Coordinates,	X_Coordinates,Available,Passenger_capacity,	Gearbox,Color,Brand,Model,Type,	Fuel_Type,Plate_number,FK1_Address)
 VAlUES (39.361888, 22.950119, TRUE, 5, FALSE, "BMW", "iX","Red","Suv", "Electric", "TTR6545", "Volos");
